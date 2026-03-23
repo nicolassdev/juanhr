2255e0808a1c9bfaa4f7fd8aad193dabb8d384e6
Anthony <anthonydaen25@gmail.com>
2026-03-23 23:07:27 +0800

    update db

--- backend/prisma/migrations/20260321225444_init/migration.sql
+++ backend/prisma/migrations/20260321225444_init/migration.sql
@@ -95,6 +95,7 @@ CREATE TABLE `schedules` (
     `created_by` INTEGER NULL,
     `deleted_at` DATETIME(3) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `late_rule` VARCHAR(10) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
